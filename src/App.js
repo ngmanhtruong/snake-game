01c599040afbcd6a17fa11ebea3d8f23b02f7cf6
Truong Nguyen <ng.manhtruong1996@gmail.com>
2021-10-05 23:49:17 +0700

    Finish basic funtions of the game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,421 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialSnake = {
+  head: {},
+  tail: []
+}
+const initialLevel = [
+  {value: 'easy', label: 'Easy'},
+  {value: 'medium', label: 'Medium'},
+  {value: 'hard', label: 'Hardcord'},
+]
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      rows: 10,
+      cols: 10,
+      grid: [],
+      snake: initialSnake,
+      foodPos: {},
+      speed: 300,
+      direction: '',
+      die: false,
+      isMove: true,
+      score: 0,
+      scoreFactor: 1,
+      victory: false,
+      level: 'easy',
+    };
+    this.handleKey = this.handleKey.bind(this);
+  } 
+
+  moveable(bool){
+    if(bool === true){
+      this.setState({ isMove : true});
+    }
+    if(bool === false){
+      this.setState({ isMove: false});
+    }
+  }
+
+  randomFood(){
+    // console.log('called');
+    let newRow = Math.floor((Math.random() * this.state.rows));
+    let newCol = Math.floor((Math.random() * this.state.cols));
+    let newPos = {col: newCol, row: newRow};
+    if (!this.state.snake.tail.includes(newPos))  
+      return newPos;
+    else{
+      return this.randomFood();
+    }
+  }
+
+  snakeEatFood(){
+    this.setState({ foodPos : this.randomFood() })
+  }
+
+  snakeMove(){
+    this.setState((state) => {
+      let { 
+        direction,
+        snake,
+        foodPos,
+        die,
+        isMove
+      } = state;
+
+      const {row, col} = state.snake.head;
+      let head = {row, col}; //use destructing of head above to new head
+
+      let {tail} = snake;
+
+
+      //Check if die or not to remove interval
+      if (state.die){
+        clearInterval(window.tickFnInterval);
+      }
+
+      //Snake eat food
+      if (head.row === foodPos.row && head.col === foodPos.col){
+        this.snakeEatFood();
+    
+        //longer at tail
+        let items = [...tail];
+        let item = {col: head.col, row: head.row};
+        items.unshift(item);
+        tail = items;
+      }
+
+      //SNAKE MOVE
+      let snakeBody = [...tail];
+      let newHead = {col, row};
+
+
+      switch (direction){
+        case 'LEFT':
+          if(head.col === 0){
+            newHead = head;
+            head.col = state.cols - 1;
+            break;
+          }
+          else{        
+            newHead = head; 
+            head.col--;
+          break;
+          }
+        
+        case 'UP':
+          if(head.row === 0){
+            newHead = head;
+            head.row = state.rows -1;
+            break;
+          }
+          else{
+            newHead = head;
+            head.row--;
+            break;
+          }
+
+  
+        case 'RIGHT':
+          if(head.col === state.cols - 1){
+            newHead = head;
+            head.col = 0;
+            break;
+          }
+          else {
+            newHead = head;
+            head.col++;
+            break;
+          }
+  
+        case 'DOWN':
+          if(head.row === state.rows - 1){
+            newHead = head;
+            head.row = 0;
+            break;
+          }
+          else{
+            newHead = head;
+            head.row++;
+            break;
+          }
+
+        default:
+          newHead = head;
+          break;
+      }
+
+      //push the new head then remove the tail
+      snakeBody.push(newHead);
+      snakeBody.shift();
+
+
+      
+      const newState = {
+        ...state, //previous state
+        foodPos,
+        snake:{
+          head,
+          tail : snakeBody, // make tail = snakeBody
+        }
+      }
+
+      //update grid to see new css (the snake and food)
+      const grid = this.resetGrid(newState);
+      //update score
+      const score = (newState.snake.tail.length - 1) * newState.scoreFactor;
+
+      //return new state
+      return {
+        ...newState,
+        grid,
+        score,
+        die
+      }
+    })
+
+  }
+
+  handleKey(e){
+    let { isMove } = this.state;
+    if(isMove){
+      let { direction } = this.state;
+      switch(e.keyCode){
+        case 65:
+          if(direction === 'RIGHT')
+            break;
+          else {
+            direction = 'LEFT';
+            this.moveable(false);
+            break;
+          }
+  
+        case 87:
+          if (direction === 'DOWN')
+            break;
+          else{
+            direction = 'UP';
+            this.moveable(false);
+            break;
+          }
+  
+        case 68:
+          if (direction === 'LEFT')
+            break;
+          else{
+            direction = 'RIGHT';
+            this.moveable(false);
+            break;
+          }
+  
+        case 83: 
+          if(direction === 'UP'){
+            break;
+          }
+          else{
+            direction = 'DOWN';
+            this.moveable(false);
+            break;
+          }
+      }
+      
+      const newState = {
+        ...this.state,
+        direction,
+      }
+      this.setState(state=>{
+        return {
+          ...newState
+        }
+      })
+    }
+
+  }
+
+  resetGrid(state = {}){
+    if(!Object.keys(state).length){
+      state = this.state;
+    }
+
+    const {
+      rows,
+      cols,
+      foodPos,
+      snake
+    } = state;
+
+    const arr = [];
+    for (let row = 0; row < rows; row ++){
+      for (let col = 0; col < cols; col++){
+        const food = (foodPos.row === row && foodPos.col === col);
+        const head = (snake.head.row === row && snake.head.col === col);
+        let isTail = false;
+        snake.tail.forEach(item => {// isTail equals tail length
+          if(item.row === row && item.col === col)
+            isTail = true
+        })
+        arr.push({
+          row: row,
+          col: col, // can be written as row, col
+          food: food,
+          head: head,
+          isTail
+        })
+      }
+    }
+    this.setState({grid : arr});
+  }
+
+
+
+  //Check If Collapse
+  checkCollapse(){
+    let { head } = this.state.snake;
+    let { die } = this.state;
+    let snakeBody = [...this.state.snake.tail];
+    snakeBody.pop();
+    snakeBody.forEach(part=>{
+      if(head.col === part.col && head.row === part.row){
+        die = true;
+        this.gameOver();
+      }
+    })
+  }
+
+  gameOver(){
+    alert(`Game Over. Snake's length: ${this.state.snake.tail.length}`);
+    this.setState(state=>{
+      const newState = {
+        ...state,
+        foodPos: this.randomFood(),
+        snake: {
+          head:{
+            row: 0,
+            col: 0
+          },
+          tail: [{col: 0, row: 0}]
+        }
+      }
+
+      const grid = this.resetGrid(newState);
+
+      return {
+        ...newState,
+        grid,
+      }
+    })
+  }
+
+  gameVictory(condition=99){
+    if(this.state.snake.tail.length == condition){
+      this.setState({ victory : true })
+    }
+  }
+
+  handleSelectChange = (e) =>{
+    let value = e.target.value
+    this.setState({ level : value });
+    if (value === 'easy'){
+      this.gameVictory(50);
+      this.setState({ speed: 300 });
+    }
+    if (value === 'medium'){
+      this.gameVictory(70);
+      this.setState({ speed: 100 });
+    }
+    if (value === 'hard'){
+      this.gameVictory(90);
+      this.setState({ speed: 1 });
+    }
+  }
+
+  //Update Grid, handle interval
+  componentDidMount(){
+    this.setState(state=>{
+      const newState = {
+        ...state,
+        foodPos: this.randomFood(),
+        snake: {
+          head:{
+            row: 0,
+            col: 0
+          },
+          tail: [{col: 0, row: 0}]
+        }
+      }
+
+      const grid = this.resetGrid(newState);
+
+      return {
+        ...newState,
+        grid,
+      }
+    })
+
+    //handle key press event
+    document.body.addEventListener('keydown', this.handleKey);
+
+    //set timeTick
+    window.tickFnInterval = setInterval(()=>{
+      this.moveable(true);
+      this.snakeMove();
+    }, this.state.speed);
+  }
+
+  componentDidUpdate(){
+    this.checkCollapse();
+  }
+
+  componentWillUnmount(){
+    document.body.removeEventListener('keydown', this.handleKey);
+    clearInterval(window.tickFnInterval);
+    this.checkCollapse();
+  }
+
+  render(){
+    const { grid } = this.state;
+    return (
+      <div className="App">
+        <h1>SNAKE GAME</h1>
+        <h3 className='start'>PRESS W,A,S,D KEYS TO START</h3>
+        <div className="grid-container">
+          {grid && grid.map(item=>(
+              <div
+                key={`${item.row.toString()} - ${item.col.toString()}`}
+                className={
+                  item.head ? 'grid-item is-head' 
+                  : item.isTail ? 'grid-item is-tail'
+                  : item.food ? 'grid-item is-food' :'grid-item'}
+              >
+              </div>
+            )
+          )}
+        <div className='info'>
+            <div className='score'>
+              <h3>SCORE</h3>
+              <p>{this.state.score}</p>
+            </div>
+            <div className='change-level'>
+              <h3>CHANGE DIFFICULT</h3>
+              <select value={this.state.level} onChange={this.handleSelectChange}>
+                {initialLevel.map(option=>(
+                  <option value={option.value}>{option.label}</option>
+                ))}
+              </select>
+            </div>
+            <div className='win-condition'>
+              <h3>WIN CONDITION</h3>
+              <p>Score: {this.state.level === 'easy' ? '50' 
+                : this.state.level ==='medium' ? '70' : '90'}
+              </p>
+            </div>
+        </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
